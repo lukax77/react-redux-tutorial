a2e56df9baeb02bca15d38b72bc675e5728749ea
Lucas <lucasducculi@gmail.com>
2016-10-01 17:47:44 -0300

    Initial push

---
+++ public/scripts/polyfill.js
@@ -0,0 +1,27 @@
+/**
+ * We will need the JavaScript Object.assign method, so I also added a
+ * polyfill that defines the method in the absence of browser support.
+ */
+if (typeof Object.assign != 'function') {
+  (function () {
+    Object.assign = function (target) {
+      'use strict';
+      if (target === undefined || target === null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var output = Object(target);
+      for (var index = 1; index < arguments.length; index++) {
+        var source = arguments[index];
+        if (source !== undefined && source !== null) {
+          for (var nextKey in source) {
+            if (source.hasOwnProperty(nextKey)) {
+              output[nextKey] = source[nextKey];
+            }
+          }
+        }
+      }
+      return output;
+    };
+  })();
+}
